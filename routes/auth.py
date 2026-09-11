c9b8e2a0b3f74528e8aca07bffc6c73e36a08df1
Dhananjay Sharma <dhanjipc0002@gmail.com>
2026-09-11 23:09:51 +0530

    Redirect logout to login page for faster response

--- routes/auth.py
+++ routes/auth.py
@@ -109,4 +109,4 @@ def login():
 def logout():
     logout_user()
     flash("You have been logged out.", "info")
-    return redirect(url_for("main.index"))
+    return redirect(url_for("auth.login"))
